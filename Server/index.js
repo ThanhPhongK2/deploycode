28060c01049431224a387952e9c5a31055289b39
Phong <phong044756@gmail.com>
2025-07-18 16:16:51 +0700

    phongdeptrai

--- Server/index.js
+++ Server/index.js
@@ -11,7 +11,8 @@ import UploadRoute from './Routes/UploadRoute.js';
 
 // Routes
 const app = express();
-
+dotenv.config();
+const port = process.env.PORT || 5000;
 
 // to serve images for public (public folder)
 app.use(express.static('public'));
@@ -23,20 +24,26 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-dotenv.config();
 
-mongoose.connect
-    (process.env.MONGO_DB, { useNewUrlParser: true, useUnifiedTopology: true }
-    ).then(() =>
-        app.listen(process.env.PORT, () => console.log(`listening at ${process.env.PORT}`))
-    ).catch((error) =>
-        console.log('error')
-    )
+const dbUri = process.env.MONGO_DB;
 
-
+async function connect() {
+    try {
+        await mongoose.connect(process.env.MONGO_DB);
+        console.log('MongoDB Connected!');
+    }
+    catch (error) {
+        console.log("MongoDB Connection Error:", error);
+    }
+}
+connect()
 // uses of routes
 
 app.use('/auth', AuthRoute);
 app.use('/user', UserRoute);
 app.use('/post', PostRoute);
 app.use('/upload', UploadRoute);
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
